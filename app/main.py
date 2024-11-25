d7da21360e22167429d1b026ddd60eaa676f4591
Surya Prakash <suryaprakash.bollu@accelyzei.com>
2024-11-25 17:51:32 +0530

    fixed sonar issue

--- app/main.py
+++ app/main.py
@@ -7,5 +7,5 @@ def read_root():
     return {"message": "Welcome to your FastAPI application!"}
 
 @app.get("/items/{item_id}")
-def readItem(item_id: int, q: str = None):
+def read_tem(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
